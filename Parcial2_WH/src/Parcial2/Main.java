47301e7060f84f3a0958a073fc2edd3f5efbd6a4
royvicente90 <r.roy@correo.unimete.edu.ve>
2020-12-09 16:17:12 -0400

    Lo que llevo

---
+++ Parcial2_WH/src/Parcial2/Main.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parcial2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author royvi
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+         AsociacionVecinos AsoVen = new AsociacionVecinos();
+ //Leer archivo .CSV con los datos dados por el profesor.
+ //Ubicación de archivo: ..DirectorioDeProyecto/test/
+    
+ BufferedReader bufferLectura = null;
+ try {
+  // Abrir el .csv en buffer de lectura
+  bufferLectura = new BufferedReader(new FileReader("test/datosparcial.csv"));
+  
+  // Leer una linea del archivo
+  String linea = bufferLectura.readLine();
+  
+  while (linea != null) {
+   // Separardor de lineas
+   String[] campos = linea.split(","); 
+   
+   AsoVen.agregar(campos[0],campos[1],campos[2],campos[3],campos[4]);
+   // Volver a leer otra línea del fichero
+   linea = bufferLectura.readLine();
+  }
+ } 
+ catch (IOException e) {
+  e.printStackTrace();
+ }
+ finally {
+  // Cierro el buffer de lectura
+  if (bufferLectura != null) {
+   try {
+    bufferLectura.close();
+   } 
+   catch (IOException e) {
+    e.printStackTrace();
+   }
+  }
+ }
+        //Main como tál
+        
+        Interfaz interfaz = new Interfaz();
+        interfaz.show();
+    }
+
+}
